e6d92badc8c985e67257ad5f87e7c6e53b21e967
Sebastián Delgao <sebasdeldi@hotmail.com>
2016-11-29 20:52:58 -0500

    make it better

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,21 @@
+class PagesController < ApplicationController
+	skip_before_action :verify_authenticity_token, only: [:info]
+	protect_from_forgery :except => :info
+	layout "second"
+
+
+	def info
+		if params[:initzarqkr].present?
+			@init = params[:initzarqkr]
+		  	@h1 = params[:h]
+		  	@a1 = params[:a]
+		end
+
+		if params[:trigger].present?
+			@trigger = params[:trigger]
+			@h2 = params[:h0]
+			@a2 = params[:a0]
+		end
+	end
+end
+
--- db/migrate/20161013043202_add_user_to_forms.rb
+++
@@ -1,5 +0,0 @@
-class AddUserToForms < ActiveRecord::Migration
-  def change
-    add_reference :forms, :user, index: true, foreign_key: true
-  end
-end

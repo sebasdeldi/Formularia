36011f9a01e49d1e27c11c1510252d02ee876083
Sebastián Delgao <sebasdeldi@hotmail.com>
2016-12-04 15:18:25 -0500

    fxd2

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :forms
   post 'forms/new'
   post 'forms/show', as: 'questions_answers'
-  post '/', to: "pages#info"
+  post '/cr7', to: "pages#info"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

b2c6ac5a2691d6ac2a08cc21e45db2b3f5ddbd3e
Sebastián Delgao <sebasdeldi@hotmail.com>
2016-12-04 12:33:27 -0500

    adds added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  @random_ad = rand(1..19)
+
 end

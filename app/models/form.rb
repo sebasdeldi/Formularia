f4c51f5d3d3ea9da650a897f1a41689b779f05e3
Sebastián Delgao <sebasdeldi@hotmail.com>
2016-12-07 14:05:28 -0500

    jjjj

--- app/models/form.rb
+++ app/models/form.rb
@@ -10,8 +10,16 @@
 #
 
 class Form < ActiveRecord::Base
+	before_create :randomize_id
 	has_many :questions, dependent: :destroy
 	belongs_to :user
 	accepts_nested_attributes_for :questions, :reject_if => lambda { |a| a[:body].blank? }
+end
 
+
+private
+def randomize_id
+  begin
+    self.id = SecureRandom.random_number(1_000_000)
+  end while Model.where(id: self.id).exists?
 end
